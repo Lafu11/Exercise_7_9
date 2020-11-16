2b2cbc252e5cea8577ef0d8775485810a5eb1471
LAFU <clafugarcia@gmail.com>
2020-11-16 18:08:57 +0100

    Average exercise with cleared and explained code.

--- average.cs
+++ average.cs
@@ -1,12 +1,13 @@
 using System;
+// calculates the average of a set of marks, until the user types “end”.
 public class Average{
 	public static void Main(){
 		double n1 = 0, n2, count = 0, average;
 		string CheckEnd;
 		Console.Write("Insert a mark: ");
-		CheckEnd = Console.ReadLine();	// we read the mark as string to check the word "end"	 
+		CheckEnd = Console.ReadLine();
 		while(CheckEnd != "end"){
-			n2 = Convert.ToDouble(s);
+			n2 = Convert.ToDouble(CheckEnd);
 			count++;
 			n1 += n2;
 			Console.Write("Insert a mark: ");
